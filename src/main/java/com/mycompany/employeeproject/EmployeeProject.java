2d8453dd400cc2c71e183720736cd086e5204af1
Teledi_Makhafola <77184819+teledi-makhafola@users.noreply.github.com>
2023-03-15 15:07:15 +0200

    first init commit

---
+++ src/main/java/com/mycompany/employeeproject/EmployeeProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.employeeproject;
+
+/**
+ *
+ * @author damacm1121
+ */
+public class EmployeeProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
